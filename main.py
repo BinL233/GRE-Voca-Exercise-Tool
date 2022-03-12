d42bdcc58fc21586046a23484912f3cd58a7666c
BinL233 <835741923@qq.com>
2022-03-13 02:38:36 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+import openpyxl
+import random
+
+work = openpyxl.load_workbook('3000.xlsx')
+incor = openpyxl.load_workbook('IncorrectList.xlsx')
+
+
+sheet1_in = incor['Sheet1']
+
+
+sheet1 = work['3000']
+print("Max row: " + sheet1.dimensions) 
+
+min = int(input("Choose the min position of words list: "))
+max = int(input("Choose the max position of words list: "))
+
+for x in range(min, max + 1):
+    value = random.randint(min, max)
+    ques = sheet1["I" + str(value)].value
+    print(ques)
+
+    input('')
+    ans = sheet1["L" + str(value)].value
+    print(ans)
+
+    ifcorrect = input('Is your answer correct? [Y/N]: ')
+    print('\n')
+    if ifcorrect == 'Y':
+        pass
+
+    else:
+        row_value = [ques, ans]
+        sheet1_in.append(row_value)
+        incor.save('IncorrectList.xlsx')
+
